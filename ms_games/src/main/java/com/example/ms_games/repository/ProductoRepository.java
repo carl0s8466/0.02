b4704596c6921f29888a5c63bcff94f9d7d361c3
carl0s8466 <138223317+carl0s8466@users.noreply.github.com>
2025-05-08 09:04:22 -0500

    Add files via upload

---
+++ ms_games/src/main/java/com/example/ms_games/repository/ProductoRepository.java
@@ -0,0 +1,7 @@
+package com.example.ms_games.repository;
+
+import com.example.ms_games.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+}
